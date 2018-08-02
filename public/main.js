4bb88c74af14d34777855b5e87e04c95b0910386
Dino Ribeiro <DRibeiro1124@gmail.com>
2018-08-02 15:41:31 -0400

    added some basic css

--- public/main.js
+++ public/main.js
@@ -1,5 +1,25 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
+// const main = () => {
+//   document.querySelector('h1').textContent += '?'
+// }
+
+// document.addEventListener('DOMContentLoaded', main)
+
+const deck = []
+const suitOfCards = ['spades', 'hearts', 'diamonds', 'clubs']
+const rankOfCards = ['ace', 'deuce', 'trey', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king']
+
+for (let i = 0; i < suitOfCards.length; i++) {
+  for (let j = 0; j < rankOfCards.length; j++) {
+    deck.push(rankOfCards[j] + ' of ' + suitOfCards[i])
+  }
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const shuffleButtonClickEvent = () => {
+  const card = deck.pop()
+
+  // Build a list item and append it to the list in the page.
+
+  console.log(card)
+}
+
+document.querySelector('#shuffle-button').addEventListener('click', shuffleButtonClickEvent)
